1e684999bf2a04ef995f336727f38e3c2a1f7d62
jescalantegt <jescalantegt@gmail.com>
2016-03-18 17:47:16 -0600

    Correccion en carga de foundation

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,7 +33,6 @@ module.exports = function(config) {
       'bower_components/fastclick/lib/fastclick.js',
       'bower_components/jquery.cookie/jquery.cookie.js',
       'bower_components/jquery-placeholder/jquery.placeholder.js',
-      'bower_components/foundation/js/foundation.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
